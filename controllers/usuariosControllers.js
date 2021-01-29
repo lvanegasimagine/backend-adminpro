7a990779e70fdbd25d133aaa292defad2f663596
Luis Vanegas <lvanegas1429@gmail.com>
2021-01-29 16:37:36 -0600

    validacion del Correo registrado o duplicado

--- controllers/usuariosControllers.js
+++ controllers/usuariosControllers.js
@@ -1,4 +1,5 @@
 const Usuario = require('../models/usuario');
+const { response } = require('express');
 
 const getUsuarios = async(req, res) => {
 
@@ -14,20 +15,41 @@ const getUsuarios = async(req, res) => {
     });
 };
 
-const crearUsuarios = async(req, res) => {
+const crearUsuarios = async(req, res = response) => {
 
     //El req es lo que manda el usuario
 
-    // const { email, password, nombre } = req.body;
+    const { email, password, nombre } = req.body;
 
-    const usuario = new Usuario(req.body);
+    try {
+        // Esta linea captura el email del body
+        const existeEmail = await Usuario.findOne({ email });
 
-    await usuario.save();
+        if (existeEmail) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Correo ya registrado'
+            });
+        };
 
-    res.json({
-        ok: true,
-        usuario
-    });
+        const usuario = new Usuario(req.body);
+
+        await usuario.save();
+
+        res.json({
+            ok: true,
+            usuario
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Error inesperado revisar log'
+        });
+    }
+
+
 };
 
 module.exports = {
