9502042846b6eb9dc54199ea695979f0272c3e89
Luis Vanegas <lvanegas1429@gmail.com>
2021-01-30 09:05:59 -0600

    Finalizando el PUT con exito

--- controllers/usuariosControllers.js
+++ controllers/usuariosControllers.js
@@ -1,6 +1,7 @@
 const Usuario = require('../models/usuario');
 const { response } = require('express');
-const { validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const usuario = require('../models/usuario');
 
 
 const getUsuarios = async(req, res) => {
@@ -21,16 +22,7 @@ const crearUsuarios = async(req, res = response) => {
 
     //El req es lo que manda el usuario
 
-    const { email, password, nombre } = req.body;
-
-    const errores = validationResult(req);
-
-    if (!errores.isEmpty()) {
-        return res.status(400).json({
-            ok: false,
-            errors: errores.mapped()
-        });
-    }
+    const { email, password } = req.body;
 
     try {
         // Esta linea captura el email del body
@@ -45,6 +37,11 @@ const crearUsuarios = async(req, res = response) => {
 
         const usuario = new Usuario(req.body);
 
+        // Encriptar Contraseña
+        const salt = bcrypt.genSaltSync();
+        usuario.password = bcrypt.hashSync(password, salt);
+
+        //Guardar Usuario
         await usuario.save();
 
         res.json({
@@ -63,7 +60,58 @@ const crearUsuarios = async(req, res = response) => {
 
 };
 
+const actualizarUsuarios = async(req, res = response) => {
+
+    //TODO Validar token y comprobar si es el usuario correcto
+
+    const uid = req.params.id;
+    try {
+
+        const usuarioDB = await usuario.findById(uid);
+
+        if (!usuarioDB) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'No existe el usuario por ese id'
+            });
+        }
+
+        const campos = req.body;
+
+        if (usuarioDB.email === req.body.email) {
+            delete campos.email;
+        } else {
+            const existeEmail = await Usuario.findOne({ email: req.body.email });
+            if (existeEmail) {
+                res.status(400).json({
+                    ok: false,
+                    msg: 'Ya existe un usuario con ese email'
+                })
+            }
+        }
+        //Actualizaciones 
+        delete campos.password;
+        delete campos.google;
+
+        const usuarioActualizado = await Usuario.findByIdAndUpdate(uid, campos, { new: true });
+
+
+        res.json({
+            ok: true,
+            usuario: usuarioActualizado
+        })
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Error Inesperado'
+        });
+    }
+}
+
 module.exports = {
     getUsuarios,
-    crearUsuarios
+    crearUsuarios,
+    actualizarUsuarios
 }
--- routes/usuariosRoutes.js
+++ routes/usuariosRoutes.js
@@ -3,16 +3,25 @@
 */
 
 const { Router } = require('express');
-const { getUsuarios, crearUsuarios } = require('../controllers/usuariosControllers');
+const { getUsuarios, crearUsuarios, actualizarUsuarios } = require('../controllers/usuariosControllers');
 const { check } = require('express-validator');
+const { validarCampos } = require('../middlewares/validar-campos');
 
 const router = Router();
 
 router.get('/', getUsuarios);
+
 router.post('/', [
     check('nombre', 'El nombre es obligatorio').not().isEmpty(),
     check('password', 'La contraseña es obligatorio').not().isEmpty(),
-    check('email', 'El email es obligatorio').isEmail()
+    check('email', 'El email es obligatorio').isEmail(),
+    validarCampos
 ], crearUsuarios);
+
+router.put('/:id', [
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    check('email', 'El email es obligatorio').isEmail(),
+    check('role', 'El role es obligatorio').not().isEmpty()
+], actualizarUsuarios);
 
 module.exports = router;
