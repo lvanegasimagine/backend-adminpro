6a1510bcc1c51d08dc128270d5200e615a195d35
Luis Vanegas <lvanegas1429@gmail.com>
2021-02-01 14:59:31 -0600

    Busqueda Global(usuario, Medico, Hospital) Expresiones Regulares

--- controllers/busquedaControllers.js
+++ controllers/busquedaControllers.js
@@ -1,13 +1,26 @@
 const { response } = require('express');
 
-const getTodo = (req, res = response) => {
+const Usuario = require('../models/usuario');
+const Medico = require('../models/medico');
+const Hospital = require('../models/hospital');
+
+const getTodo = async(req, res = response) => {
 
     const busqueda = req.params.busqueda;
+    const regex = new RegExp(busqueda, 'i');
+
+    const [usuarios, medico, hospital] = await Promise.all([
+        Usuario.find({ nombre: regex }),
+        Medico.find({ nombre: regex }),
+        Hospital.find({ nombre: regex })
+    ]);
 
     try {
         res.status(200).json({
             ok: true,
-            msg: busqueda
+            usuarios,
+            medico,
+            hospital
         });
     } catch (error) {
         console.log(error);
